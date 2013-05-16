f821f2b02e1a038a0b00a044bbd3c04f0aac478e
mpurham <marcellpurham@gmail.com>
2013-05-16 11:43:12 -0500

    solved problem #2

--- problem-2.rb
+++ problem-2.rb
@@ -1,17 +1,5 @@
-#Fibonacci = [0,1]
-#Fibonacci.each_with_index { |f, i| sum =  (f - f[i]) + (f - f[1]); puts "#{f} is equal => #{i}" }
-#Fibonacci.each_with_index { |f, i| sum = (f[i-1] + f[i-2]); puts sum }
+#its a bit messy but it works
+puts 55.times.inject([0,1]) {|s| s + [s[-1] + s[-2]] }.select { |sel| sel % 2 == 0 && sel <= 4000000 }.inject() {|sum, x| sum + x}
 
-#puts 10.times.inject([0,1]) {|s| s + [s[-1] + s[-2]]}
-
-# def fib(n)
-# 	f,i = 0,1
-# 	n.times do 
-# 	  puts n[i-1] + n[i-2]
-# 	end
-# end
-
-# fib(5)
-# Fibonacci = (1..10).to_a
-# #Fibonacci.each_with_index { |f, i| sum =  (f - f[i]) + (f - f[1]); puts "#{f} is equal => #{i}" }
-# Fibonacci.each_with_index { |f, i| sum =  (f - f[0]) + (f - f[1]); puts sum += f[i-1] }
+# a = (1..100).to_a
+# puts a.select { |sel| sel % 2 == 0 }
