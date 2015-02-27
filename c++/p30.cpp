dcd9decab29eaaa3686d00747276426567d65bbc
Marcell Purham <mpurham@10-107-209-216.wireless.uic.edu>
2015-02-27 11:49:07 -0600

    working on problem 30

---
+++ c++/p30.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+//using namespace std;
+
+int main()
+{
+//    std::cout << number << endl;
+        
+/*
+    for(int i = 9999; 0 < i; i-- )
+    {
+        if( i % 4 == 0 )
+        {
+            printf("\n");
+        }
+        printf("%d ", i);
+    }
+*/    
+    /*
+    int value = 9474;
+    int sum = value;
+    int a,b,c,d;   
+     
+    std::cout << value << std::endl; 
+    std::cout << value % 10  << std::endl;    
+
+    a = value % 10;
+    value /= 10; 
+    std::cout << value % 10  << std::endl;
+
+    b = value % 10;
+    value /= 10; 
+    std::cout << value % 10 << std::endl;
+    
+    c = value % 10;
+    value /= 10; 
+    std::cout << value % 10 << std::endl;
+    
+    d = value % 10;
+
+    //std::cout << a << " " << b << " " << c << " " << d << " " << std::endl;
+    */
+
+    /* 
+    if( (pow(a,4) + pow(b,4) + pow(c, 4) + pow(d, 4) ) == sum )
+        std::cout << "Success" << std::endl; 
+    */
+/*
+    int arr[5];
+    int total = 0;
+
+    for(int i=0; i <= 500000; i++)
+    {
+        int value2 = i;
+        int a,b,c,d,e; 
+        int j = 0;
+
+        // MOD EACH TIME
+        while( value2 >= 10 )
+        {
+            //std::cout << value2 % 10 << std::endl;
+            arr[j++] = value2 % 10;  
+           value2 /= 10; 
+        }
+        arr[j] = value2 % 10;
+        //std::cout << value2 % 10 << std::endl;
+        if ( (pow(arr[0],5) + pow(arr[1],5) + pow(arr[2],5) + pow(arr[3], 5) + pow(arr[4], 5) ) == i )
+        {
+            total+=i;
+            std::cout << i << std::endl;
+        }
+    }
+    
+    std::cout << total << std::endl;
+*/
+
+}
